5e6ee0ebb48400da8186edc5ceb5feb836ea8816
Starovoytov Alexey <relz0071@gmail.com>
2017-10-06 11:04:09 +0000

    Добавлен скелет сервера с поддержкой маршрутизации

---
+++ config.ts
@@ -0,0 +1,5 @@
+export const CONFIG: {
+	PORT: number
+} = {
+	PORT: 1507
+};
---
+++ server.ts
@@ -0,0 +1,27 @@
+import * as cors from 'cors';
+import * as express from 'express';
+import { CONFIG } from './config';
+
+const app: express.Express = express();
+
+app.use(cors());
+
+app.get('/', () => {
+	console.log('Hello from GET');
+});
+
+app.post('/', () => {
+	console.log('Hello from POST');
+});
+
+app.put('/', () => {
+	console.log('Hello from PUT');
+});
+
+app.delete('/', () => {
+	console.log('Hello from DELETE');
+});
+
+app.listen(CONFIG.PORT, () => {
+	console.log(`Express server listening on port ${CONFIG.PORT}`);
+});
