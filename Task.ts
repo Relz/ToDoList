8bd07a7027647f1fc9a5540c61ff21b8f8e26cc5
term96@yandex.ru <ohd0yzki>
2017-10-07 21:46:18 +0300

    Создать класс задания пользователя Сущность задания пользователя (Task): id: number title: string description: string creationDate: Date deadline: Date isDone: boolean userId: number

---
+++ Task.ts
@@ -0,0 +1,9 @@
+export class Task {
+	public id: number;
+	public title: string;
+	public description: string;
+	public creationDate: Date;
+	public deadline: Date;
+	public isDone: boolean;
+	public userId: number;
+}
