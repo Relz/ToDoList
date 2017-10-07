3e89e9482212d58b9795db7cccf4a095515ad7b5
term96@yandex.ru <ohd0yzki>
2017-10-08 01:28:43 +0300

    Добавлены сеттеры\геттеры

--- Task.ts
+++ Task.ts
@@ -1,9 +1,65 @@
 export class Task {
-	public id: number;
-	public title: string;
-	public description: string;
-	public creationDate: Date;
-	public deadline: Date;
-	public isDone: boolean;
-	public userId: number;
+	private _id: number;
+	private _title: string;
+	private _description: string;
+	private _creationDate: Date;
+	private _deadline: Date;
+	private _isDone: boolean;
+	private _userId: number;
+
+	get id(): number {
+		return this._id;
+	}
+
+	set id(value: number) {
+		this._id = value;
+	}
+
+	get title(): string {
+		return this._title;
+	}
+
+	set title(value: string) {
+		this._title = value;
+	}
+
+	get description(): string {
+		return this._description;
+	}
+
+	set description(value: string) {
+		this._description = value;
+	}
+
+	get creationDate(): Date {
+		return this._creationDate;
+	}
+
+	set creationDate(value: Date) {
+		this._creationDate = value;
+	}
+
+	get deadline(): Date {
+		return this._deadline;
+	}
+
+	set deadline(value: Date) {
+		this._deadline = value;
+	}
+
+	get isDone(): boolean {
+		return this._isDone;
+	}
+
+	set isDone(value: boolean) {
+		this._isDone = value;
+	}
+
+	get userId(): number {
+		return this._userId;
+	}
+
+	set userId(value: number) {
+		this._userId = value;
+	}
 }
