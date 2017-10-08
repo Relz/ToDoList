327179919f7ca0c2dd82f4252b936c06d0dca1d9
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2017-10-08 10:06:03 +0300

    Геттеры-Сеттеры

--- User.ts
+++ User.ts
@@ -1,6 +1,34 @@
 export class User {
-	public id: number;
-	public login: string;
-	public password: string;
-	public name: string;
+	private _id: number;
+	private _login: string;
+	private _password: string;
+	private _name: string;
+
+	get id(): number {
+		return this._id;
+	}
+	set id(value: number) {
+		this._id = value;
+	}
+
+	get login(): string {
+		return this._login;
+	}
+	set login(value: string) {
+		this._login = value;
+	}
+
+	get password(): string {
+		return this._password;
+	}
+	set password(value: string) {
+		this._password = value;
+	}
+
+	get name(): string {
+		return this._name;
+	}
+	set name(value: string) {
+		this._name = value;
+	}
 }
