bef631069c8997b6a62c353f3f863f3b22b7e0f3
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2017-10-08 10:10:56 +0300

    Геттеры и сеттеры сделаны публичными

--- User.ts
+++ User.ts
@@ -4,31 +4,31 @@ export class User {
 	private _password: string;
 	private _name: string;
 
-	get id(): number {
+	public get id(): number {
 		return this._id;
 	}
-	set id(value: number) {
+	public set id(value: number) {
 		this._id = value;
 	}
 
-	get login(): string {
+	public get login(): string {
 		return this._login;
 	}
-	set login(value: string) {
+	public set login(value: string) {
 		this._login = value;
 	}
 
-	get password(): string {
+	public get password(): string {
 		return this._password;
 	}
-	set password(value: string) {
+	public set password(value: string) {
 		this._password = value;
 	}
 
-	get name(): string {
+	public get name(): string {
 		return this._name;
 	}
-	set name(value: string) {
+	public set name(value: string) {
 		this._name = value;
 	}
 }
