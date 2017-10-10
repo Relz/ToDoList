ffeb3c296a8cf6297ba3610db13e3e79e5b88808
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2017-10-10 19:23:38 +0300

    Автосборка проекта

---
+++ Server/src/ts/JsonResponse.ts
@@ -0,0 +1,20 @@
+export class JsonResponse {
+	private _responseCode: number;
+	private _response: Object;
+
+	public get responseCode(): number {
+		return this._responseCode;
+	}
+
+	public set responseCode(value: number){
+		this._responseCode = value;
+	}
+
+	public get response(): Object {
+		return this._response;
+	}
+
+	public set response(value: Object){
+		this._response = value;
+	}
+}
