d2777a21cb5844eae2c3d779bb95ad58f1b690aa
Starovoytov Alexey <relz0071@gmail.com>
2017-10-11 00:36:57 +0000

    Spaces to tabs

--- Server/gruntfile.js
+++ Server/gruntfile.js
@@ -1,62 +1,61 @@
-module.exports = function(grunt)
-{
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-        ts: {
-            default: {
-                src: ['src/ts/*.ts'],
-                outDir: 'build/',
-                options: {
-                    configFile: 'tsconfig.json'
-                }
-            }
-        },
+		ts: {
+			default: {
+				src    : ['src/ts/*.ts'],
+				outDir : 'build/',
+				options: {
+					configFile: 'tsconfig.json'
+				}
+			}
+		},
 
-        tslint: {
-            options:
-                {
-                    configFile: 'tslint.json'
-                },
-            validate: ['src/**/*.ts']
-        },
+		tslint: {
+			options :
+				{
+					configFile: 'tslint.json'
+				},
+			validate: ['src/**/*.ts']
+		},
 
-        clean: {
-            build: {
-                build: ['build'],
-                tscache: ['.tscache'],
-                logs: ['*.log']
-            }
-        },
+		clean: {
+			build: {
+				build  : ['build'],
+				tscache: ['.tscache'],
+				logs   : ['*.log']
+			}
+		},
 
-        shell: {
-            cspell: {
-                command: 'cspell src/**/*.ts',
-                options: {
-                    stderr: true
-                }
-            },
-            run_server: {
-                command: 'node build/server.js',
-                options: {
-                    stderr: true
-                }
-            }
-        }
-    });
+		shell: {
+			cspell    : {
+				command: 'cspell src/**/*.ts',
+				options: {
+					stderr: true
+				}
+			},
+			run_server: {
+				command: 'node build/server.js',
+				options: {
+					stderr: true
+				}
+			}
+		}
+	});
 
-    grunt.loadNpmTasks('grunt-ts'),
-    grunt.loadNpmTasks('grunt-tslint');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('grunt-ts');
+	grunt.loadNpmTasks('grunt-tslint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-shell');
 
-    grunt.registerTask('build', [
-        'clean:build',
-        'ts',
-        'shell:run_server'
-    ]);
-    grunt.registerTask('validate', [
-        'tslint',
-        'shell:cspell'
-    ]);
+	grunt.registerTask('build', [
+		'clean:build',
+		'ts',
+		'shell:run_server'
+	]);
+	grunt.registerTask('validate', [
+		'tslint',
+		'shell:cspell'
+	]);
 };
