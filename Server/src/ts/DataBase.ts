3f02d4e7cf07f004a46f78d42a22f33d9009f9a7
term96@yandex.ru <ohd0yzki>
2017-10-14 23:08:19 +0300

    Реализовать аутентификацию пользователя по логину и паролю метод: POST ссылка: /users/authenticate входные параметры: логин и md5 хэш пароля возвращает: токен

--- Server/src/ts/DataBase.ts
+++ Server/src/ts/DataBase.ts
@@ -2,7 +2,26 @@ import { Config } from './Config';
 import * as sqlite3 from 'sqlite3';
 
 export class DataBase {
-	private static db = new sqlite3.Database(Config.dbName);
+	public static readonly userNotExists = -1;
+	public static readonly wrongPassword = -2;
+
+	private static db = new sqlite3.Database(Config.dbName, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err: Error) => {
+		if (err) {
+			throw err;
+		}
+	});
+
+	public static getUserId(login: string, password: string, callBack: (err, rowId) => any): void {
+		DataBase.db.get('SELECT * FROM user WHERE login = ?', login, (err: Error, row: any) => {
+			if (!row) {
+				callBack(err, DataBase.userNotExists);
+			} else if (password !== row.password) {
+				callBack(err, DataBase.wrongPassword);
+			} else {
+				callBack(err, row.id);
+			}
+		});
+	}
 
 	public static getUsers(): any[] {
 		let result: any[] = [];
@@ -20,7 +39,7 @@ export class DataBase {
 	public static init(): void {
 		DataBase.db.run('CREATE TABLE IF NOT EXISTS user (' +
 			'id INTEGER PRIMARY KEY,' +
-			'login VARCHAR,' +
+			'login VARCHAR UNIQUE,' +
 			'password VARCHAR,' +
 			'name VARCHAR);', (err: Error) => {
 				if (err) {
--- Server/src/ts/server.ts
+++ Server/src/ts/server.ts
@@ -1,11 +1,16 @@
 import * as cors from 'cors';
 import * as express from 'express';
-import { Config } from './Config';
+import * as bodyParser from 'body-parser'
 import { DataBase } from './DataBase';
+import { Config } from './Config';
+import { JsonResponse } from './JsonResponse';
+import { Token } from './Token';
 
 const app: express.Express = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 DataBase.init();
 
 app.get('/', () => {
@@ -15,6 +20,46 @@ app.get('/users', () => {
 });
 
 app.post('/', () => {
+});
+
+app.post('/users/authenticate', (req, res) => {
+	let responseJson: JsonResponse = new JsonResponse;
+
+	try {
+		if (!req.body || !req.body.login || !req.body.password) {
+			responseJson.responseCode = 400;
+			responseJson.response = { message: 'Request should contain a login and a password' };
+			return res.status(responseJson.responseCode).send(responseJson);
+		}
+
+		const login = req.body.login;
+		const password = req.body.password;
+		// Here will be checks for correctness of login and password
+
+		DataBase.getUserId(login, password, (err, id) => {
+			if (err) {
+				throw err;
+			}
+			if (id === DataBase.userNotExists) {
+				responseJson.responseCode = 403;
+				responseJson.response = { message: 'The user does not exist' };
+			} else if (id === DataBase.wrongPassword) {
+				responseJson.responseCode = 403;
+				responseJson.response = { message: 'The password is wrong' };
+			} else {
+				const payload = { user_id: id };
+				const token = Token.create(payload);
+				responseJson.responseCode = 200;
+				responseJson.response = { token: token };
+			}
+			res.status(responseJson.responseCode).send(responseJson);
+		});
+	} catch (error) {
+		responseJson.responseCode = 500;
+		responseJson.response = { message: 'Something went wrong' };
+		res.status(responseJson.responseCode).send(responseJson);
+		// Here will be logging
+	}
 });
 
 app.put('/', () => {
