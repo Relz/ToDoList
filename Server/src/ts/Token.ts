2ade1745c5e88df0f8c6790dfb818606eeba27c5
term96@yandex.ru <ohd0yzki>
2017-10-15 15:24:58 +0300

    Исправления по пул-реквесту №12

--- Server/src/ts/Token.ts
+++ Server/src/ts/Token.ts
@@ -2,7 +2,7 @@ import * as jwt from 'jsonwebtoken';
 import { Config } from './config';
 
 export class Token {
-	static create(tokenData: any) {
+	static create(tokenData: any): any {
 		return jwt.sign(tokenData, Config.secret, { expiresIn: Config.tokenLifeTime });
 	}
 }
--- Server/src/ts/server.ts
+++ Server/src/ts/server.ts
@@ -1,12 +1,19 @@
 import * as cors from 'cors';
 import * as express from 'express';
-import * as bodyParser from 'body-parser'
+import * as bodyParser from 'body-parser';
 import { DataBase } from './DataBase';
 import { Config } from './Config';
 import { JsonResponse } from './JsonResponse';
 import { Token } from './Token';
 
 const app: express.Express = express();
+
+enum ResponseStatus {
+	OK = 200,
+	BAD_REQUEST = 400,
+	FORBIDDEN = 403,
+	INTERNAL_SERVER_ERROR = 500
+}
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,38 +34,32 @@ app.post('/users/authenticate', (req, res) => {
 
 	try {
 		if (!req.body || !req.body.login || !req.body.password) {
-			responseJson.responseCode = 400;
-			responseJson.response = { message: 'Request should contain a login and a password' };
+			responseJson.responseCode = ResponseStatus.BAD_REQUEST;
 			return res.status(responseJson.responseCode).send(responseJson);
 		}
 
 		const login = req.body.login;
 		const password = req.body.password;
-		// Here will be checks for correctness of login and password
 
 		DataBase.getUserId(login, password, (err, id) => {
 			if (err) {
 				throw err;
 			}
-			if (id === DataBase.userNotExists) {
-				responseJson.responseCode = 403;
-				responseJson.response = { message: 'The user does not exist' };
-			} else if (id === DataBase.wrongPassword) {
-				responseJson.responseCode = 403;
-				responseJson.response = { message: 'The password is wrong' };
+			if (id === DataBase.Result.USER_NOT_EXISTS) {
+				responseJson.responseCode = ResponseStatus.FORBIDDEN;
+			} else if (id === DataBase.Result.WRONG_PASSWORD) {
+				responseJson.responseCode = ResponseStatus.FORBIDDEN;
 			} else {
 				const payload = { user_id: id };
 				const token = Token.create(payload);
-				responseJson.responseCode = 200;
+				responseJson.responseCode = ResponseStatus.OK;
 				responseJson.response = { token: token };
 			}
 			res.status(responseJson.responseCode).send(responseJson);
 		});
 	} catch (error) {
-		responseJson.responseCode = 500;
-		responseJson.response = { message: 'Something went wrong' };
+		responseJson.responseCode = ResponseStatus.INTERNAL_SERVER_ERROR;
 		res.status(responseJson.responseCode).send(responseJson);
-		// Here will be logging
 	}
 });
 
