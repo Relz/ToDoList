a050bca3ab3829187444d431db737e220b80e56e
Starovoytov Alexey <relz0071@gmail.com>
2017-10-15 16:51:16 +0000

    Добавить конструктор классу Task для полного заполнения его свойств Конструктор принимает все свойства, тем самым после создания объекта не потребуется дополнительная настройка

--- Server/src/ts/Task.ts
+++ Server/src/ts/Task.ts
@@ -2,10 +2,38 @@ export class Task {
 	private _id: number;
 	private _title: string;
 	private _description: string;
-	private _creationDate: Date;
-	private _deadline: Date;
+	private _creationDate: number;
+	private _deadline: number;
 	private _isDone: boolean;
 	private _userId: number;
+
+	constructor()
+	constructor(
+		id: number,
+		title: string,
+		description: string,
+		creationDate: number,
+		deadline: number,
+		isDone: boolean,
+		userId: number
+	)
+	constructor(
+		id?: number,
+		title?: string,
+		description?: string,
+		creationDate?: number,
+		deadline?: number,
+		isDone?: boolean,
+		userId?: number
+	) {
+		this._id = id;
+		this._title = title;
+		this._description = description;
+		this._creationDate = creationDate;
+		this._deadline = deadline;
+		this._isDone = isDone;
+		this._userId = userId;
+	}
 
 	public get id(): number {
 		return this._id;
@@ -31,19 +59,19 @@ export class Task {
 		this._description = value;
 	}
 
-	public get creationDate(): Date {
+	public get creationDate(): number {
 		return this._creationDate;
 	}
 
-	public set creationDate(value: Date) {
+	public set creationDate(value: number) {
 		this._creationDate = value;
 	}
 
-	public get deadline(): Date {
+	public get deadline(): number {
 		return this._deadline;
 	}
 
-	public set deadline(value: Date) {
+	public set deadline(value: number) {
 		this._deadline = value;
 	}
 
