44b64420e2b24e6a768a435340827cb850712a74
Starovoytov Alexey <relz0071@gmail.com>
2017-10-18 11:11:59 +0000

    Добавлен метод верификации токена

--- Server/src/ts/Token.ts
+++ Server/src/ts/Token.ts
@@ -5,4 +5,8 @@ export class Token {
 	static create(tokenData: any): string {
 		return jwt.sign(tokenData, Config.secret, { expiresIn: Config.tokenLifeTime });
 	}
+
+	static verify(token: string): any {
+		return jwt.verify(token, Config.secret);
+	}
 }
