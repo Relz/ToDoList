0c77a1da6c836cc3bd1a14838bb0e6229ffa317e
term96 <term96@yandex.com>
2017-10-21 09:13:51 +0300

    Реализовать редактирование пользователя (#19)
    * Реализовать редактирование пользователя
    метод: PUT
    ссылка: /users/edit
    входные параметры: токен
    новые данные в x-www-form-urlencoded
    
    * Удалены ненужные проверки из БД, проверки на существование переменных сделаны более человекочитаемыми
    
    * Использование User в editUser (БД), инициализация User через конструктор, switch-case вместо if-else

--- Server/src/ts/server.ts
+++ Server/src/ts/server.ts
@@ -1,12 +1,13 @@
 import * as cors from 'cors';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import { DataBase } from './DataBase';
+import { DataBase, DbResult } from './DataBase';
 import { Config } from './Config';
 import { JsonResponse } from './JsonResponse';
 import { Token } from './Token';
 import { UserInfo } from './UserInfo';
 import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
+import { User } from './User';
 
 const app: express.Express = express();
 app.use(cors());
@@ -66,10 +67,10 @@ app.post('/users/authenticate', (req: express.Request, res: express.Response) =>
 
 		DataBase.getUserId(login, password, (id: number) => {
 			let httpStatus: number;
-			if (id === DataBase.Result.USER_NOT_EXISTS) {
+			if (id === DbResult.USER_NOT_EXISTS) {
 				jsonResponse.responseCode = 2;
 				httpStatus = ResponseStatus.FORBIDDEN;
-			} else if (id === DataBase.Result.WRONG_PASSWORD) {
+			} else if (id === DbResult.WRONG_PASSWORD) {
 				jsonResponse.responseCode = 3;
 				httpStatus = ResponseStatus.FORBIDDEN;
 			} else {
@@ -86,6 +87,57 @@ app.post('/users/authenticate', (req: express.Request, res: express.Response) =>
 });
 
 app.put('/', () => {
+});
+
+
+app.put('/users/edit/:token', (req: express.Request, res: express.Response) => {
+	let jsonResponse: JsonResponse = new JsonResponse;
+	try {
+		let userId: number;
+
+		try {
+			userId = Token.verify(req.params.token).id;
+		} catch (err) {
+			jsonResponse.responseCode = 5;
+			return res.status(ResponseStatus.FORBIDDEN).send(jsonResponse);
+		}
+
+		if (!req.body || !req.body.login || !req.body.name || !req.body.password) {
+			jsonResponse.responseCode = 1;
+			return res.status(ResponseStatus.BAD_REQUEST).send(jsonResponse);
+		}
+
+		let newData: User = new User(
+			userId,
+			req.body.login,
+			(req.body.newPassword !== undefined) ? req.body.newPassword : req.body.password,
+			req.body.name);
+
+		DataBase.editUser(userId, req.body.password, newData, (dbResult: number) => {
+			let responseStatus: number;
+			switch (dbResult) {
+				case DbResult.USER_NOT_EXISTS:
+					jsonResponse.responseCode = 2;
+					responseStatus = ResponseStatus.FORBIDDEN;
+					break;
+				case DbResult.WRONG_PASSWORD:
+					jsonResponse.responseCode = 3;
+					responseStatus = ResponseStatus.FORBIDDEN;
+					break;
+				case DbResult.LOGIN_IN_USE:
+					jsonResponse.responseCode = 6;
+					responseStatus = ResponseStatus.FORBIDDEN;
+					break;
+				default:
+					jsonResponse.responseCode = 0;
+					responseStatus = ResponseStatus.OK;
+			}
+			return res.status(responseStatus).send(jsonResponse);
+		});
+	} catch (err) {
+		jsonResponse.responseCode = 4;
+		res.status(ResponseStatus.INTERNAL_SERVER_ERROR).send(jsonResponse);
+	}
 });
 
 app.delete('/', () => {
