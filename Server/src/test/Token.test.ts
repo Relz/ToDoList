b307a376a71900aec04d979eab6773ff9eb88440
term96@yandex.ru <ohd0yzki>
2017-11-05 21:17:14 +0300

    Исправлены замечания; исправлена работа expect'ов

--- Server/src/test/Token.test.ts
+++ Server/src/test/Token.test.ts
@@ -3,14 +3,23 @@ import { Token } from '../ts/Token/Token';
 
 describe('Token class', () => {
 	const id: number = 1337;
+	const invalidToken: string = 'invalid_token';
 	let token: string;
-	it('can create token', () => {
-		expect(token = Token.createFromId(id)).not.to.throw;
-		expect(token).not.to.be.undefined.and.not.to.be.empty;
+	describe('createFromId()', () => {
+		it('creates token from id', () => {
+			expect(Token.createFromId.bind(null, id)).not.to.throw();
+			token = Token.createFromId(id);
+			expect(token).not.to.be.undefined;
+		});
 	});
-	it('can decode id', () => {
-		let decodedId: number;
-		expect(decodedId = Token.decodeId(token)).not.to.throw;
-		expect(decodedId).to.be.equal(id);
+	describe('decodeId()', () => {
+		it('returns id decoded from token', () => {
+			expect(Token.decodeId.bind(null, token)).not.to.throw();
+			const decodedId: number = Token.decodeId(token);
+			expect(decodedId).to.be.equal(id);
+		});
+		it('throws error on invalid token', () => {
+			expect(Token.decodeId.bind(null, invalidToken)).to.throw();
+		});
 	});
 });
