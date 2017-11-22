4b63d15981869bb237c89cd6448e8cac7a1a4e04
Starovoytov Alexey <relz0071@gmail.com>
2017-11-22 18:54:39 +0000

    Упс

--- Client/src/Component/Button/Button.tsx
+++ Client/src/Component/Button/Button.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { IButtonProps } from '../Props/IButtonProps';
 import { ButtonType } from './ButtonType';
-import { ButtonSize } from './ButtonSize';
 import * as classNames from 'classnames';
 import { IButtonState } from '../State/IButtonState';
+import { Utils } from '../../Utils/Utils';
 
 export class Button extends React.Component<IButtonProps, IButtonState> {
 	public constructor() {
@@ -29,19 +29,6 @@ export class Button extends React.Component<IButtonProps, IButtonState> {
 		const classes: string = classNames({
 			button: true,
 			stretch: true,
-			basic: this.state.type === ButtonType.Basic,
-			default: this.state.type === ButtonType.Default,
-			primary: this.state.type === ButtonType.Primary,
-			success: this.state.type === ButtonType.Success,
-			info: this.state.type === ButtonType.Info,
-			warning: this.state.type === ButtonType.Warning,
-			danger: this.state.type === ButtonType.Danger,
-			link: this.state.type === ButtonType.Link,
-
-			extra_small: this.props.size === ButtonSize.ExtraSmall,
-			small: this.props.size === ButtonSize.Small,
-			medium: this.props.size === ButtonSize.Medium,
-			large: this.props.size === ButtonSize.Large,
 
 			enabled: !disabled,
 			disabled: disabled
@@ -54,7 +41,10 @@ export class Button extends React.Component<IButtonProps, IButtonState> {
 
 		return (
 			<button
-				className={classes}
+				className={classes + Utils.toClassNames(
+					this.state.type,
+					this.props.size
+				)}
 				disabled={disabled}
 				onClick={onClick}
 			>
