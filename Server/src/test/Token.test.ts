976544af69043eee2d945d42fb5bb17457daaabf
term96@yandex.ru <ohd0yzki>
2017-11-26 13:52:10 +0300

    Указаны аргументы в названиях функций bind'ы заменены на arrow functions

--- Server/src/test/Token.test.ts
+++ Server/src/test/Token.test.ts
@@ -5,21 +5,21 @@ describe('Token class', () => {
 	const id: number = 1337;
 	const invalidToken: string = 'invalid_token';
 	let token: string;
-	describe('createFromId()', () => {
+	describe('createFromId(id: number)', () => {
 		it('creates token from id', () => {
-			expect(Token.createFromId.bind(null, id)).not.to.throw();
+			expect(() => { Token.createFromId(id); }).not.to.throw();
 			token = Token.createFromId(id);
 			expect(token).not.to.be.undefined;
 		});
 	});
-	describe('decodeId()', () => {
+	describe('decodeId(token: string)', () => {
 		it('returns id decoded from token', () => {
-			expect(Token.decodeId.bind(null, token)).not.to.throw();
+			expect(() => { Token.decodeId(token); }).not.to.throw();
 			const decodedId: number = Token.decodeId(token);
 			expect(decodedId).to.be.equal(id);
 		});
 		it('throws error on invalid token', () => {
-			expect(Token.decodeId.bind(null, invalidToken)).to.throw();
+			expect(() => { Token.decodeId(invalidToken); }).to.throw();
 		});
 	});
 });
